70eefde9c744f787cdd155ce5cefb4ca62e0f62c
PhilG <philg.github@gmail.com>
2013-06-09 14:07:33 -0400

    rework tests and implement build method

--- lib/tifi/changelog.rb
+++ lib/tifi/changelog.rb
@@ -3,9 +3,40 @@ class Changelog
     
     def initialize(options)
         @options = options
+        @entries = []
     end
     
     def build
+        repo = Grit::Repo.new("#{@options[:base]}.")
+        
+        tags = Grit::Tag.find_all(repo).reverse
+        
+        tags.each do |t|
+            e = Entry.new(t.name, [t.commit])
+          
+            repo.commits(e.tag).each do |c|
+                unless c.message == t.commit.message
+                    unless already? c && c.message =~ /\[(F|B|I)\]/i
+                        e.commits << c
+                    end
+                end
+            end           
+                        
+            @entries << e
+            
+        end
+    end
+    
+    def already?(commit)
+        if @entries == []
+            return false
+        else
+            @entries.each do |e|
+                if (e.commits.include? commit)
+                    return true
+                end
+            end
+        end            
     end
     
     def result
--- test/tifi/changelog_spec.rb
+++ test/tifi/changelog_spec.rb
@@ -1,6 +1,6 @@
 describe Changelog do
     
-    let(:options) { { :io => "Changelog.md" } }
+    let(:options) { { :io => "Changelog.md", :base => "test/repo/" } }
     let(:subject) { Changelog.new(options) }
     
     it "is able to build" do
@@ -10,12 +10,25 @@ describe Changelog do
     it "has entries" do
         subject.must_respond_to :entries
     end
-    
-    it "can output the result" do
-        proc { Changelog.new({ :io => $stdout }).result }.must_output "# Changelog #\n"
-    end
-    
-    it "finishes properly given STDOUT" do
+        
+    it "finishes properly given $stdout as IO" do
         proc { Changelog.new({ :io => $stdout }).finish }.must_output "# Changelog #\n"
     end
+    
+    it "has ALL the tags" do
+        subject.build
+        subject.entries.each do |e|
+            e.must_be_instance_of Entry
+            e.commits[0].must_be_instance_of Grit::Commit
+        end
+    end
+    
+    it "builds a valid entries array" do
+        skip
+        subject.build
+        subject.entries.each do |e|
+            e.message.must_match /\[(F|B|I)\]/i
+        end
+    end
+
 end
